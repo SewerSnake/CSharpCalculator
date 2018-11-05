2f8a29f7e19978147c03f02237eb4981696b6ad9
Eric Groseclos <ericgroseclos@hotmail.com>
2018-11-05 16:52:47 +0100

    Basic functionality for tasks c and e have been implemented.

--- CSharpCalculator/CSharpCalculator/Program.cs
+++ CSharpCalculator/CSharpCalculator/Program.cs
@@ -2,16 +2,30 @@
 
 namespace CSharpCalculator
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("1. Add two numbers");
+            Console.WriteLine("2. Multiply two numbers");
+            Console.WriteLine("3. Sum the numbers between two numbers.");
+            Console.WriteLine("4. Add number to memory");
+            Console.WriteLine("5. Clear memory");
+            Console.WriteLine("6. Print calculator contents.");
+            Console.WriteLine("7. Quit");
         }
     }
 
     class Calculator 
     {
-        // This is our calculator class.
+        void SumNumbers()
+        {
+            // return FirstNumber + SecondNumber;
+        }
+
+        void MultiplyNumbers()
+        {
+            // return FirstNumber * SecondNumber;
+        }
     }
 }
